c3a1f8bd5a8e3934f279da0db66d173d44c39ebd
Abdelkarim Djelalda <abdelkarimdjelalda@protonmail.com>
2022-11-25 21:45:52 +0100

    listen on $PORT

--- backend/app.ts
+++ backend/app.ts
@@ -23,6 +23,6 @@ app.post('/api/v1/shorten', shortenerRoute);
 app.get('/:shortCode', getUrl)
 
 
-app.listen(3000, () => {
+app.listen(env.PORT || 3000, () => {
   console.log('Server is running...')
 });
